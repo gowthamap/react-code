603bcb2439a0fd837f65f69a9ae793235ab36b56
Gowthamap <gowthamap55555@gmail.com>
2021-12-01 23:11:11 +0530

    components

---
+++ src/list/Label.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import './Label.css';
+
+class Label extends react.Component {
+    render() {
+        return <span className="list-label-item">Label KK</span>
+    }
+}
+
+export default Label;
